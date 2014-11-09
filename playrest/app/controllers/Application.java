da5167764ede5cb1238665cb3ab06a939470e064
Joaquin y Esther <casajoyth@Mac-mini-de-Joaquin.local>
2014-11-09 01:45:09 +0100

    Rest dummy receptor added

--- playrest/app/controllers/Application.java
+++ playrest/app/controllers/Application.java
@@ -1,14 +1,26 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
+	}
+
+	/**
+	 * REST method used to receive a dummy json object
+	 * 
+	 * @return
+	 */
+	@BodyParser.Of(BodyParser.Json.class)
+	public static Result receiveDummy() {
+		JsonNode json = request().body().asJson();
+		return ok("JSON Dummy received");
+	}
 
 }
